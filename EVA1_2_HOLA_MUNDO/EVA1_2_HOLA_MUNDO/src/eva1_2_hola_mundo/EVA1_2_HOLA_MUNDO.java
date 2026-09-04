b673cfbe4f069701338efacff3efa17bd01283e3
JoahnMbappe <L26550208@chihuahua2.tecnm.mx>
2026-09-03 18:35:14 -0600

    Add files via upload

---
+++ EVA1_2_HOLA_MUNDO/EVA1_2_HOLA_MUNDO/src/eva1_2_hola_mundo/EVA1_2_HOLA_MUNDO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_hola_mundo;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_2_HOLA_MUNDO {
+    
+    public static void main(String[] args) { //INICIO DE PROGRAMA
+        //ESTO ES UN BLOQUE DE CÓDIGO
+        //MOSTRAR UN MENSAJE DE TEXTO
+        //MOSTRAR--> INSTRUCCION
+        //TEXTO-->DATOS
+        System.out.println("Hola Mundo!!");
+    }//FIN DE PROGRAMA
+    
+}
