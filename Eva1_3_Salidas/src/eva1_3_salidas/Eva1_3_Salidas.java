5499b61d5685c2e4b3f728e69d46837984f0fd3a
JoahnMbappe <L26550208@chihuahua2.tecnm.mx>
2026-09-03 18:44:13 -0600

    Add files via upload

---
+++ Eva1_3_Salidas/src/eva1_3_salidas/Eva1_3_Salidas.java
@@ -0,0 +1,19 @@
+
+package eva1_3_salidas;
+
+
+public class Eva1_3_Salidas {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Bienvenido a Fundamentos de Programación");
+        System.out.println("Tarango De Santiago Joahn Haziel");
+        System.out.print("Tu Calificación sera: ");
+        System.out.println(85);
+    }
+}
+
+    
+    
+
