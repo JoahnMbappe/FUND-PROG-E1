7a008917ca7c580334b2eb1c94ba534fb634bd44
JoahnMbappe <L26550208@chihuahua2.tecnm.mx>
2026-09-14 18:52:56 -0600

    Add files via upload

---
+++ Eva1_9_Operaciones_cadenas/src/eva1_9_operaciones_cadenas/Eva1_9_Operaciones_cadenas.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_operaciones_cadenas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class Eva1_9_Operaciones_cadenas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Tambien hay operaciones para cadenas
+        //concatenacion: pegar cadenas de texto --> +
+        String cade;
+        cade = "Hola" + " " + "Mundo";
+        System.out.println(cade);
+        cade = "Valor = " + 100;
+        System.out.println(cade);
+        cade = "Valores Booleanos = " + false;
+        System.out.println(cade);
+    }
+    
+}
