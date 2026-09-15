edad7eabd7dc168607043644392e8a27abdacd4e
JoahnMbappe <L26550208@chihuahua2.tecnm.mx>
2026-09-14 18:52:38 -0600

    Add files via upload

---
+++ Eva1_8_Operaciones_2/src/eva1_8_operaciones_2/Eva1_8_Operaciones_2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_operaciones_2;
+
+/**
+ *
+ * @author bisonte
+ */
+public class Eva1_8_Operaciones_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int resu, val1 = 10, val2= 5; //declaro variable e inicializando
+        //java es buena onda: inicializa las variables
+        //numero en cero, booleanos en falso
+        //niveles de operadores.
+        resu = 100 + val1 * (val1/val2);
+        System.out.println("resu = "+ resu);
+        
+    }
+    
+}
